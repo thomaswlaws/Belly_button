9cedbd28479c8f29b0fc928cd20b5eb97695994a
unknown <tinytom118@gmail.com>
2020-07-17 17:54:56 -0500

    made changes

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://sqlite:///db/bellybutton.sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
 db = SQLAlchemy(app)
 
 # reflect an existing database into a new model
@@ -99,10 +99,7 @@ def samples(sample):
         "otu_labels": sample_data.otu_label.tolist(),
     }
     return jsonify(data)
-try:
-   int('')
-except ValueError:
-   pass      # or whatever
+
 
 if __name__ == "__main__":
     app.run()
--- static/js/app.js
+++ static/js/app.js
@@ -1,75 +1,174 @@
+function buildGauge(wfreq){
+
+  // pie chart converted to gauge chart
+  let traceGauge = {
+    type: 'pie',
+    showlegend: false,
+    hole: 0.4,
+    rotation: 90,
+    values: [180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180],
+    text: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
+    direction: 'clockwise',
+    textinfo: 'text',
+    textposition: 'inside',
+    marker: {
+      colors: ['#F8F3EC','#F4F1E5','#E9E6CA','#E2E4B1','#D5E49D','#B7CC92','#8CBF88','#8ABB8F','#85B48A','white'],
+      labels: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9',''],
+      hoverinfo: "label"
+    },
+    hoverinfo: "skip"
+  }
+
+  // the dot where the needle "originates"
+  let dot = {
+    type: 'scatter',
+    x: [0],
+    y: [0],
+    marker: {
+      size: 14,
+      color:'#850000'
+    },
+    showlegend: false,
+    hoverinfo: "skip"
+  }
+
+  // the needle (triangular version)
+
+    // add weights to the degrees to correct needle
+  let weight = 0;
+  if (wfreq == 2 || wfreq == 3){
+    weight = 3;
+  } else if (wfreq == 4){
+    weight = 1;
+  } else if (wfreq == 5){
+    weight = -.5;
+  } else if (wfreq == 6){
+    weight = -2;
+  } else if (wfreq == 7){
+    weight = -3;
+  }
+
+  let degrees = 180-(20 * wfreq + weight); // 20 degrees for each of the 9 gauge sections
+  let radius = .5;
+  let radians = degrees * Math.PI / 180;
+  let aX = 0.025 * Math.cos((radians) * Math.PI / 180);
+  let aY = 0.025 * Math.sin((radians) * Math.PI / 180);
+  let bX = -0.025 * Math.cos((radians) * Math.PI / 180);
+  let bY = -0.025 * Math.sin((radians) * Math.PI / 180);
+  let cX = radius * Math.cos(radians);
+  let cY = radius * Math.sin(radians);
+
+  // draw the triangle
+  let path = 'M ' + aX + ' ' + aY +
+            ' L ' + bX + ' ' + bY +
+            ' L ' + cX + ' ' + cY +
+            ' Z';
+
+  let gaugeLayout = {
+    title: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week",
+    shapes:[{
+        type: 'path',
+        path: path,
+        fillcolor: '#850000',
+        line: {
+          color: '#850000'
+        }
+      }],
+    xaxis: {zeroline:false, 
+            showticklabels:false,
+            showgrid: false, 
+            range: [-1, 1],
+            fixedrange: true
+          },
+    yaxis: {zeroline:false, 
+            showticklabels:false,
+            showgrid: false, 
+            range: [-1, 1],
+            fixedrange: true
+          }
+  };
+
+  Plotly.newPlot("gauge", [traceGauge, dot], gaugeLayout);
+}
+
 async function buildMetadata(sample) {
 
   // Use `d3.json` to fetch the metadata for a sample
   const url = "/metadata/" + sample;
   let data = await d3.json(url);
 
-  // Use d3 to select the panel with the id of `#sample-metadata`
+  // Use d3 to select the panel with id of `#sample-metadata`
   let panel = d3.select('#sample-metadata');
 
   // Use `.html("") to clear any existing metadata
   panel.html("");
 
-  // Use `Object.entries` to add each a key and a value pair to the panel
-  let data_pairs= Object.entries(data);
+  // Use `Object.entries` to add each key and value pair to the panel
+  let data_pairs = Object.entries(data);
   data_pairs.forEach(pair => panel.append("text").text(pair[0] + ": " + pair[1] + "\n").append("br"));
+
+  // BONUS: Build the Gauge Chart
+  buildGauge(data.WFREQ);
+  
 }
-  function buildCharts(sample) {
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
-  const url_sampleData = `/samples/${sample}`
-  d3.json(url_sampleData).then((response)=>{
+async function buildCharts(sample) {
 
-    const otu_ids = response.otu_ids;
-    const otu_labels = response.otu_labels;
-    const sample_values = response.sample_values;
-    // @TODO: Build a Bubble Chart using the sample data
-    const bubble_layout = {
-      margin: { t: 0 },
-      hovermode: "closest",
-      xaxis: { title: "OTU ID" }
-    };
-    const bubble_data = [
-      {
-        x: otu_ids,
-        y: sample_values,
-        text: otu_labels,
-        mode: "markers",
-        marker: {
-          size: sample_values,
-          color: otu_ids,
-          colorscale: "Earth"
-        }
+  // Use `d3.json` to fetch the sample data for the plots
+  const url = "/samples/" + sample;
+  let data = await d3.json(url);
+
+  // Build a Pie Chart using top 10 values for sample_values, 
+  // otu_ids, and labels (10 each).
+  let sample_values = data.sample_values;
+  let otu_ids = data.otu_ids;
+  let otu_labels = data.otu_labels;
+
+  let pie_data = {
+    values: sample_values.slice(0,10),
+    labels: otu_ids.slice(0,10),
+    type: 'pie',
+    hovertext: otu_labels.slice(0,10)
+  }
+
+  let pie_layout = {
+    title: "Top 10 OTU_ID Counts"
+  }
+
+  Plotly.newPlot("pie", [pie_data], pie_layout);
+  
+  // Build a Bubble Chart using the sample data
+  let bubble_data = {
+    type:"scatter",
+    x: otu_ids,
+    y: sample_values,
+    mode: 'markers',
+    marker: {
+              color: otu_ids, 
+              size: sample_values.map(d => d)
+            },
+    hovertext: otu_labels
+  }
+
+  let bubble_layout = {
+    title: "OTU_IDs in Sample",
+    xaxis: {
+      title: {
+        text: 'OTU ID',
       }
-    ];
+    }
+  };
 
-    Plotly.plot("bubble", bubble_data, bubble_layout);
-    // @TODO: Build a Pie Chart
-    const pie_data = [
-      {
-        values: sample_values.slice(0, 10),
-        labels: otu_ids.slice(0, 10),
-        hovertext: otu_labels.slice(0, 10),
-        hoverinfo: "hovertext",
-        type: "pie"
-      }
-    ];
+  Plotly.newPlot("bubble", [bubble_data], bubble_layout);
 
-    const pie_layout = {
-      margin: { t: 0, l: 0 }
-    };
-
-    Plotly.plot("pie", pie_data, pie_layout);
-  });
-    
 }
 
 function init() {
   // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
+  let selector = d3.select("#selDataset");
 
-  // Use the list of sample names to populate the selected options
-    d3.json("/names").then((sampleNames) => {
+  // Use the list of sample names to populate the select options
+  d3.json("/names").then((sampleNames) => {
     sampleNames.forEach((sample) => {
       selector
         .append("option")
